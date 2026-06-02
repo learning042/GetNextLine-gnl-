9c2209e76c8b9f33f37f54cc0a245caf2eb70fd5
learning042 <individualthinking1403@gmail.com>
2026-06-02 21:50:22 +0100

    finished bonus

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <string.h>
-#include "get_next_line_bonus.h"
+#include "get_next_line.h"
 char    *get_next_line(int fd)
 {
-	static char    buffer[BUFFER_SIZE + 1];
+	static char    buffer[1024][BUFFER_SIZE + 1];
 	char        *line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
@@ -13,19 +13,27 @@ char    *get_next_line(int fd)
 	if (!line)
 		return (NULL);
 	line[0] = '\0';
-	line = ft_get_line(fd, buffer, &line);
+	line = ft_get_line(fd, buffer[fd], &line);
 	return (line);
 }
 /*
 int    main(void)
 {    
-    int    fd = open("shrek.txt", O_RDONLY);
+    int	fd = open("hello.txt", O_RDONLY);
+    int	fd2 = open("world.txt", O_RDONLY);	
     char    *line = get_next_line(fd);
     while (line != NULL)
     {
         printf("%s", line);
 	free(line);
         line = get_next_line(fd);
+    }
+char    *line2 = get_next_line(fd2);
+    while (line2 != NULL)
+    {
+        printf("%s", line2);
+	free(line2);
+        line2 = get_next_line(fd2);
     }
     return (0);
 }
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_s_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpinto-v <tpinto-v@student.42lisb...>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/27 21:59:23 by tpinto-v          #+#    #+#             */
+/*   Updated: 2026/05/27 22:30:11 by tpinto-v         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int		my_strlen(char *s, char term)
+{
+	int	len;
+
+	len = 0;
+	while (s[len] && s[len] != term)
+		++len;
+	return (len);
+}
+
+int		ft_findnl(char *s)
+{
+	int	len;
+	int	nl_index;
+
+	len = my_strlen(s, '\0');
+	nl_index = my_strlen(s, '\n');
+	if (len == nl_index)
+		return (0);
+	return (1);
+}
+
+char	*my_strjoin(char *s1, char *s2)
+{
+	char	*s_conc;
+	int		i;
+	int		j;
+
+	s_conc = malloc(my_strlen(s1, '\0') + my_strlen(s2, '\n') + 1 + ft_findnl(s2));
+	if (s_conc == NULL)
+		return (NULL);
+	i = -1;
+	j = 0;
+	while (s1[++i])
+		s_conc[i] = s1[i];
+	while (s2[j] && s2[j] != '\n')
+	{
+		s_conc[i + j] =  s2[j];
+		++j;
+	}
+	if (s2[j] == '\n')
+	{
+		s_conc[i + j] = '\n';
+		s_conc[i + j + 1] = '\0';
+	}
+	else
+		s_conc[i + j] = '\0';
+	free(s1);
+	return (s_conc);
+}
+
+void	ft_shift_buffer(char *buf)
+{
+	int	i;
+	int	nl_index;
+
+	nl_index = my_strlen(buf, '\n') + ft_findnl(buf);
+	i = 0;
+	while (buf[nl_index + i])
+	{
+		buf[i] = buf[nl_index + i];
+		++i;
+	}
+	buf[i] = '\0';
+}
+
+char	*ft_get_line(int fd, char *buf, char **s)
+{
+	int	bytes_read;
+
+	if (buf[0])
+	{
+		*s = my_strjoin(*s, buf);
+		if (!(*s))
+			return (NULL);
+		if (ft_findnl(*s))
+			return(ft_shift_buffer(buf), *s);
+	}
+	while ((bytes_read = read(fd, buf, BUFFER_SIZE)) > 0)
+	{
+		buf[bytes_read] = '\0';
+		*s = my_strjoin(*s, buf);
+		if (!(*s))
+			return (NULL);
+		if (ft_findnl(*s))
+			return (ft_shift_buffer(buf), *s);
+	}
+	buf[0] = '\0';
+	if (bytes_read < 0 || (*s)[0] == '\0')
+		return (free(*s), NULL);
+	return (*s);
+}
